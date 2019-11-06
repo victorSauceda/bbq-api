9e37170980c9828780a32eaf93f1db98ebe50435
Unloop <unloopunloop@Unloops-MBP.guest.substantial.com>
2019-11-06 15:39:01 -0800

    removed node modules

--- node_modules/serverless-bundle/scripts/config/babelJestTransform.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const babelJest = require('babel-jest');
-
-module.exports = babelJest.createTransformer({
-  presets: [require.resolve('@babel/preset-env')],
-  babelrc: false,
-  configFile: false,
-});
